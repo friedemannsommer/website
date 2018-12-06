bbdacc71498af0d7dba1d6d22050adc4ee42f88c
Friedemann Sommer <friedemannsommer@yahoo.de>
2018-12-06 18:37:20 +0100

    moved app creation

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,14 @@ use actix_web::{http, server, App};
 use constants::{APP_NAME, DEFAULT_HOST, DEFAULT_PORT, ENV_HOST, ENV_PORT};
 use std::env;
 
+fn create_app() -> App {
+    App::new()
+        .route("/", http::Method::GET, render::index)
+        .route("/contact", http::Method::GET, render::contact)
+        .route("/assets/{name}", http::Method::GET, assets::handle_request)
+        .default_resource(|r| r.with(render::not_found))
+}
+
 fn get_server_address() -> String {
     env::var(ENV_HOST)
         .unwrap_or(DEFAULT_HOST.to_string())
@@ -24,15 +32,7 @@ fn main() {
     let sys = actix::System::new(APP_NAME);
     let address = get_server_address();
 
-    server::new(|| {
-        App::new()
-            .route("/", http::Method::GET, render::index)
-            .route("/contact", http::Method::GET, render::contact)
-            .route("/assets/{name}", http::Method::GET, assets::handle_request)
-            .default_resource(|r| r.with(render::not_found))
-    }).bind(&address)
-    .unwrap()
-    .start();
+    server::new(|| create_app()).bind(&address).unwrap().start();
 
     println!("Started http server: {}", &address);
     sys.run();
