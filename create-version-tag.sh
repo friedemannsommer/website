e3d85bb3519e10968c5023e87ef193f7fcef493a
Friedemann Sommer <friedemann.sommer@advanced-store.com>
2019-01-21 15:12:47 +0100

    well i think "tags" should be signed, right?

--- create-version-tag.sh
+++ create-version-tag.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 version=$(sed -n -E 's/version\s*=\s*\"([^\"]+)\"/\1/p' Cargo.toml | head -n 1)
 branch=$(git branch | grep \* | cut -d ' ' -f2)
-git tag -a "$version" -m "website version $version"
+git tag -a "$version" -m "website version $version" -s
 git push origin "$branch" --follow-tags
