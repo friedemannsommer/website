11d6ee6f50b6841d650950ce287060ba0b7a8819
Friedemann Sommer <social@friedemannsommer.com>
2020-01-24 01:14:40 +0100

    return assets as binary

--- src/asset_request.rs
+++ src/asset_request.rs
@@ -52,11 +52,11 @@ pub fn handle_asset_request(
         .header("x-content-type-options", "nosniff")
         .header("x-download-options", "noopen")
         .status(StatusCode::OK)
-        .body(Body::from(match asset {
+        .body(Body::Binary(Vec::from(match asset {
             Asset::FontOtf => SOURCE_CODE_PRO_OTF,
             Asset::FontTtf => SOURCE_CODE_PRO_TTF,
             Asset::FontWoff => SOURCE_CODE_PRO_WOFF,
             Asset::FontWoff2 => SOURCE_CODE_PRO_WOFF2,
-        }))
+        })))
         .map_err(crate::util::map_http_err)
 }
