1a9f937e4f39d4d6dc1f1720b071cbf0c9b92e51
Friedemann Sommer <social@friedemannsommer.com>
2020-07-19 00:39:24 +0200

    replaced "data-encoding" crate with "base64"

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,4 @@
 use crypto::{digest::Digest, sha2::Sha256};
-use data_encoding::BASE64;
 use lambda_http::http;
 use lambda_runtime::error::HandlerError;
 
@@ -8,7 +7,7 @@ pub fn get_sha256_hash(value: &str) -> String {
     hasher.input_str(value);
     let mut output = vec![0; hasher.output_bytes()];
     hasher.result(&mut output);
-    BASE64.encode(&output)
+    base64::encode(&output)
 }
 
 pub fn map_http_err(err: http::Error) -> HandlerError {
